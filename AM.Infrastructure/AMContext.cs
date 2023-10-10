31bc34092c3fe938ebb2fc274d49ebea14bc41c4
Montah21 <moutye.bacha@esprit.tn>
2023-10-10 16:23:00 +0100

    moutye

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,28 @@
+﻿using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure
+{
+    internal class AMContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=AirportManagementDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Flight> flights { get; set; }
+        public DbSet<Passenger> passengers { get; set; }
+        public DbSet<Plane> planes { get; set; }
+        public DbSet<Staff> staff { get; set; }
+        public DbSet<Traveller> travellers { get; set; }
+
+
+
+
+    }
+}
